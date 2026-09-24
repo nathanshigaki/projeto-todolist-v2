ac397e59e7f935f55d260ce8de935b13400087b8
Nathan <nathanshigaki@gmail.com>
2026-09-24 10:09:23 -0400

    update properties

--- src/main/java/br/com/nathanshigaki/v2/Repository/TodoRepository.java
+++ src/main/java/br/com/nathanshigaki/v2/Repository/TodoRepository.java
@@ -3,11 +3,8 @@ package br.com.nathanshigaki.v2.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import br.com.nathanshigaki.v2.Model.Todo;
 
-@Repository
 public interface TodoRepository extends JpaRepository<Todo, Long>{
     List<Todo> findByUserId(long userId);
 }
--- src/main/java/br/com/nathanshigaki/v2/Repository/UserRepository.java
+++ src/main/java/br/com/nathanshigaki/v2/Repository/UserRepository.java
@@ -1,11 +1,8 @@
 package br.com.nathanshigaki.v2.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import br.com.nathanshigaki.v2.Model.User;
 
-@Repository
 public interface UserRepository extends JpaRepository <User, Long>{
 
 }
